18e2e5b5f8c73902971ea3ee27bd3aeeea96f9ef
Shawn Moore <sartak@bestpractical.com>
2008-04-29 01:54:33 +0000

    HTML::RewriteAttributes::Links which mimics HTML::ResolveLink

---
+++ lib/HTML/RewriteAttributes/Links.pm
@@ -0,0 +1,53 @@
+#!/usr/bin/env perl
+package HTML::RewriteAttributes::Links;
+use strict;
+use warnings;
+use base 'HTML::RewriteAttributes';
+use HTML::Tagset ();
+use URI;
+
+my %rewritable_attrs;
+
+for my $tag (keys %HTML::Tagset::linkElements) {
+    for my $attr (@{ $HTML::Tagset::linkElements{$tag} }) {
+        $rewritable_attrs{$tag}{$attr} = 1;
+    }
+}
+
+sub _should_rewrite {
+    my ($self, $tag, $attr) = @_;
+
+    return ( $rewritable_attrs{$tag} || {} )->{$attr};
+}
+
+sub _rewrite {
+    my ($self, $html, $base) = @_;
+
+    $self->{rewrite_link_base} = $base;
+
+    my $cb = sub {
+        my $uri = URI->new(shift);
+
+        $uri = $uri->abs($self->{rewrite_link_base})
+            unless defined $uri->scheme;
+
+        return $uri->as_string;
+    };
+
+    $self->SUPER::_rewrite($html, $cb);
+}
+
+# if we see a base tag, steal its href for future link resolution
+sub _start_tag {
+    my $self = shift;
+    my ($tagname, $attr, $attrseq, $text) = @_;
+
+    if ($tagname eq 'base' && defined $attr->{href}) {
+        $self->{rewrite_link_base} = $attr->{href};
+    }
+
+    $self->SUPER::_start_tag(@_);
+}
+
+1;
+
